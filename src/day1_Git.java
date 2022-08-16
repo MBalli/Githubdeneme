9c38058f59ce07fd18e70c16eb55217e986abd36
Mirac Balli <miracballi49@gmail.com>
2022-08-16 17:54:23 +0200

    V2

--- src/day1_Git.java
+++ src/day1_Git.java
@@ -1,6 +1,6 @@
 public class day1_Git {
     public static void main(String[] args) {
         System.out.printf("Hello Git 1.deneme");
-        System.out.printf("Hello Git 3.deneme");
+        System.out.printf("Hello Git 3 .deneme");
     }
 }
---
+++ src/day2_GitGithub.java
@@ -0,0 +1,18 @@
+public class day2_GitGithub {
+    /*
+    1- git init = Local repo olusturmak icin yani . git ile klasörümüzün icindeki dosyalari iliskilendirmek icin kullanilir.
+    2- git add . = Working space den dosyalari staging area ya (commitlemek icin beklenen yer) e gönderir.
+    3- git commit -m" = Staging area dan commit store a dosyalari göndrmek icin kullanilir.
+       (Commit yani Sürüm olusturulmus olur)
+     4- git push = Uzak repoya (remote - Github) a göndermek icin kullandigimiz kod.
+     Yalniz git push komutunu direkt kullanmak istersek 1 kez
+
+     git remote add origin adress
+     git push -u origin master
+
+     Not: Bu iki komutu tek seferde kullandiktan sonra ikinci commitlerim icin sadece git push kullaniriz.
+
+     */
+
+
+}
