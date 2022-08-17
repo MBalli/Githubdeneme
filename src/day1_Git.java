6bac2e839f12c2a347315f783666e5d4afe5e6ca
Mirac Balli <miracballi49@gmail.com>
2022-08-17 17:15:42 +0200

    Bvc

--- src/day1_Git.java
+++ src/day1_Git.java
@@ -2,5 +2,6 @@ public class day1_Git {
     public static void main(String[] args) {
         System.out.printf("Hello Git 1.deneme");
         System.out.printf("Hello Git 3 .deneme");
+        System.out.printf("Hello Git 4.deneme");
     }
 }
