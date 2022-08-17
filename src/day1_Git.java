2ba300a35c431162737cf04f3d037ea94ff01218
Mirac Balli <miracballi49@gmail.com>
2022-08-17 17:18:59 +0200

    CVC

--- src/day1_Git.java
+++ src/day1_Git.java
@@ -1,7 +1,8 @@
 public class day1_Git {
     public static void main(String[] args) {
-        System.out.printf("Hello Git 1.deneme");
-        System.out.printf("Hello Git 3 .deneme");
-        System.out.printf("Hello Git 4.deneme");
+        System.out.println("Hello Git 1.deneme");
+        System.out.println("Hello Git 3 .deneme");
+        System.out.println("Hello Git 4.deneme");
+        System.out.println("Hello Git 5.deneme");
     }
 }
